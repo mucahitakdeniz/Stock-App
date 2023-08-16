1e888ec281ec7755179667f2d58fefa62665ea08
Mücahit Akdeniz <mucahitakdeniz28@gmail.comgit config --global user.email mucahitakdeniz28@gmail.com>
2023-08-16 11:05:13 +0300

    stockApp-start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { grey, blueGrey } from "@mui/material/colors";
+
+
+function App() {
+ 
+  return (
+    <>
+<div>Stock App</div>
+
+    </>
+  );
+}
+
+export default App;
