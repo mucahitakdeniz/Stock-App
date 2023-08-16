6d4f3f816cc3fbd8e8cb5674be87f7186a547c9b
Mücahit Akdeniz <mucahitakdeniz28@gmail.comgit config --global user.email mucahitakdeniz28@gmail.com>
2023-08-16 13:25:04 +0300

    login-1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,99 @@
+import Avatar from "@mui/material/Avatar";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import LockIcon from "@mui/icons-material/Lock";
+import { Formik, Form } from "formik";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+
+
 const Login = () => {
-  return <div>Login</div>;
+  const loginSchema = () => {};
+  return (
+    <Container maxWidth="lg">
+      <Grid
+        container
+        justifyContent="center"
+        direction="row-reverse"
+        sx={{ height: "100vh", p: 2 }}
+      >
+        <Grid>
+          <Typography variant="h3" color="primary.dark" align="center">
+            STOCK APP
+          </Typography>
+        </Grid>
+        <Grid>
+          <Avatar
+            sx={{
+              backgroundColor: "secondary.dark",
+              m: "auto",
+              width: 40,
+              height: 40,
+            }}
+          >
+            <LockIcon />
+          </Avatar>
+          <Typography
+            variant="h4"
+            align="center"
+            mb={4}
+            color={"secondary.dark"}
+          >
+            Login
+          </Typography>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={loginSchema}
+            onSubmit={(values, action) => {
+              //Login fonksiyonu buraya yazılacak
+              action.resetForm();
+              action.setSubmitting(false);
+            }}
+          >
+            {({ handleChange, handleBlur, values, touched, errors }) => (
+              <Form>
+                <Box>
+                  <TextField
+                    label="Email"
+                    name="email"
+                    id="email"
+                    type="email"
+                    variant="outlined"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.value}
+                    error={touched.email && Boolean(errors.email)}
+                    helperText={errors.email}
+                  ></TextField>
+                  <TextField
+                    label="Password"
+                    name="password"
+                    id="password"
+                    type="password"
+                    variant="outlined"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.value}
+                    error={touched.password && Boolean(errors.password)}
+                    helperText={errors.password}
+                  ></TextField>
+                  <Button variant="contained" type="submit">
+                    Submit
+                  </Button>
+                </Box>
+              </Form>
+            )}
+          </Formik>
+          <Box>
+            <Link to="/register" xs={10} sm={7} md={6} > Hesabınız yok mu?</Link>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Login;
